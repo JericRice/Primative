cd98b190786c69fd2903d4969176d6e6f18924aa
Joe <jeric720@icloud.com>
2021-02-18 10:35:25 -0500

    Added example of underflow and overflow

--- src/com/jerictech/Main.java
+++ src/com/jerictech/Main.java
@@ -10,11 +10,22 @@ public class Main {
 
         int myMinIntValue = Integer.MIN_VALUE;
         int myMaxIntValue = Integer.MAX_VALUE;
-
         System.out.println("Int minimum value = " + myMinIntValue);
         System.out.println("Int maximum value = " + myMaxIntValue);
         System.out.println("Busted max value = " + (myMaxIntValue + 1)); // overflow
         System.out.println("Busted min value = " + (myMinIntValue - 1)); // underflow
+
+        int myMinByteValue = Byte.MIN_VALUE;
+        int myMaxByteValue = Byte.MAX_VALUE;
+        System.out.println("Byte minimum value = " + myMinByteValue);
+        System.out.println("Byte maximum value = " + myMaxByteValue);
+
+        int myMinShortValue = Short.MIN_VALUE;
+        int myMaxShortValue = Short.MAX_VALUE;
+        System.out.println("Short minimum value = " + myMinShortValue);
+        System.out.println("Short maximum value = " + myMaxShortValue);
+
+
 
 
     }
